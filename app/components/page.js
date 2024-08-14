e62c03e52779cc3db62cf360780b494ffca889e7
Vadim.Lobodin <vadim.lobodin@jetbrains.com>
2024-08-14 14:19:08 +0200

    make it better

--- app/components/page.js
+++
@@ -1,9 +0,0 @@
-import AIChatbotApp from './components/AIChatbotApp';
-
-export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <AIChatbotApp />
-        </main>
-    );
-}
